352d5878383fe4fb5117997c7ce7d3e12de178e2
dacctal <120422854+dacctal@users.noreply.github.com>
2025-10-09 19:27:20 +0000

    added deps.sh for easy install

---
+++ deps.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+DEPS=(
+    "https://github.com/mpv-player/mpv.git"
+    "https://github.com/yt-dlp/yt-dlp?tab=readme-ov-file"
+    "https://github.com/FFmpeg/FFmpeg"
+    "https://github.com/curl/curl"
+    "https://github.com/quodlibet/mutagen"
+)
