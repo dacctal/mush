e98ac59526f737e593de3f0cf0a6ab61fd8d61e9
dacctal <120422854+dacctal@users.noreply.github.com>
2025-10-11 05:06:13 +0000

    fixed weird urls

--- deps.sh
+++ deps.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 DEPS=(
-    "https://github.com/mpv-player/mpv.git"
-    "https://github.com/yt-dlp/yt-dlp?tab=readme-ov-file"
+    "https://github.com/mpv-player/mpv"
+    "https://github.com/yt-dlp/yt-dlp"
     "https://github.com/FFmpeg/FFmpeg"
     "https://github.com/curl/curl"
     "https://github.com/quodlibet/mutagen"
